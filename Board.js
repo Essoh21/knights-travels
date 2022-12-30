dc962c2ec28abe82a608f2d8ff24752b48916c9b
essohanam <essohanam.alou@imsp-uac.org>
2022-12-30 19:45:23 +0000

    add empty board and empty Vertex

---
+++ Board.js
@@ -0,0 +1,12 @@
+import Vertex from "./Vertex";
+
+class Board {
+    constructor(boardxMax, boardyMax) {
+        this.bordxMax = boardxMax;
+        this.boardyMax = boardyMax;
+    }
+    knightMoves(start, end) {
+
+    }
+
+}
---
+++ Vertex.js
@@ -0,0 +1,17 @@
+class Vertex {
+    constructor(position, moves) {
+        this.position = position;
+        this.parent = null;
+        this.one = null;
+        this.two = null;
+        this.three = null
+        this.four = null;
+        this.fife = null;
+        this.six = null;
+        this.seven = null;
+        this.eight = null;
+    }
+
+}
+
+export default Vertex;
