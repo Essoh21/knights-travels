5449df82c1b9a7fbabccd040bf4cd10b715fb228
essohanam <essohanam.alou@imsp-uac.org>
2023-01-02 22:15:52 +0000

    build tree class

--- Board.js
+++ Board.js
@@ -2,19 +2,54 @@ import Vertex from "./Vertex.js";
 
 class Board {
     constructor(boardxMax, boardyMax) {
-        this.bordxMax = boardxMax;
+        this.boardxMax = boardxMax;
         this.boardyMax = boardyMax;
-        this.root = this.buildGraphFrom()
+        // this.root = this.buildGraphFrom()
     }
 
     buildGraphFrom(position, destination) {
+        let currentVertex = new Vertex(position);
+        if (
+            currentVertex.position[0] == destination[0]
+            && currentVertex.position[1] == destination[1]
+        ) {
+            return currentVertex;
+        }
+
+
+        let possibleMoves = this.getkPossibleMovesFrom(currentVertex.position);
+        while (possibleMoves.length < 8) {
+            possibleMoves.push(null);
+        }
+
+        const vertexChildren = [];
+        for (let i = 0; i < possibleMoves.length; i += 1) {
+            vertexChildren.push(new Vertex(possibleMoves[i]));
+        }
+
+        [
+            currentVertex.one
+            , currentVertex.two
+            , currentVertex.three
+            , currentVertex.four
+            , currentVertex.fife
+            , currentVertex.six
+            , currentVertex.seven
+            , currentVertex.eight
+        ] = vertexChildren;
+        for (let i = 0; i < vertexChildren.length; i += 1) {
+            if (vertexChildren[i] === null) continue;
+            // console.log(vertexChildren[i].position)
+            //this.buildGraphFrom(vertexChildren[i].position, destination);
+        }
 
     }
+
     knightMoves(start, end) {
 
     }
 
-    checkPossibleMovesFrom(position) {
+    getkPossibleMovesFrom(position) {
         let x = position[0];
         let y = position[1];
         const allMovesFromPosition = [
@@ -23,13 +58,17 @@ class Board {
             , [x - 2, y + 1], [x - 2, y - 1]
             , [x - 1, y - 2], [x + 1, y - 2]
         ];
-        return allMovesFromPosition.filter((move) => {
-            (move[0] >= 0) && (move[0] < 8)
-                && (move[1] >= 0) && (move[1] < 8)
+        return allMovesFromPosition.filter(function (move) {
+            if ((move[0] > 0)
+                && (move[0] < 8)
+                && (move[1] > 0)
+                && (move[1] < 8)) return true;
+            return false;
         });
+
     }
 
 }
 
 const example = new Board(8, 8);
-console.log(example.checkPossibleMovesFrom([1, 2]));
+console.log(example.buildGraphFrom([0, 0], [2, 1]));
